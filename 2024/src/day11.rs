624799c019cd5e33ed71188a50d3c7330a30013a
Andres Sanchez <andsanma@amazon.com>
2024-12-11 09:29:18 +0100

    [2024] day11 (star2)

--- 2024/src/day11.rs
+++ 2024/src/day11.rs
@@ -2,77 +2,54 @@ use std::collections::HashMap;
 
 use aoc::input_file;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum Number {
-    Pure(u64),
-    Splitted(Vec<Number>),
-    // Cached(Number, u64),
-}
-
 #[derive(Debug)]
 struct Data {
-    stones: Number,
+    stones: HashMap<usize, usize>,
 }
 
 impl std::str::FromStr for Data {
     type Err = std::io::Error;
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        Ok(Data {
-            stones: Number::Splitted(
-                input
-                    .split_whitespace()
-                    .map(|line| Number::Pure(line.parse().unwrap()))
-                    .collect(),
-            ),
-        })
+        let mut stones = HashMap::new();
+        for stone_n in input.split_whitespace() {
+            let stone = stone_n.parse().unwrap();
+            *stones.entry(stone).or_default() += 1;
+        }
+
+        Ok(Data { stones })
     }
 }
 
-fn evolve_stone(stone: &Number, cache: &mut HashMap<Number, Number>) -> Number {
-    if let Some(r) = cache.get(stone) {
-        return r.clone();
-    }
-    let r: Number = {
-        match stone {
-            Number::Pure(0) => Number::Pure(1),
-            Number::Pure(stone_n) => {
-                let length_num = stone_n.ilog10() + 1 % 2;
-                if length_num % 2 == 0 {
-                    Number::Splitted(vec![
-                        Number::Pure(stone_n / 10_u64.pow(length_num / 2)),
-                        Number::Pure(stone_n % 10_u64.pow(length_num / 2)),
-                    ])
-                } else {
-                    Number::Pure(stone_n * 2024)
-                }
-            }
-            Number::Splitted(v) => {
-                Number::Splitted(v.iter().map(|s| evolve_stone(s, cache)).collect())
+fn evolve_stone(stones: &HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut new_stones = HashMap::new();
+    for (stone, count) in stones {
+        if *stone == 0 {
+            *new_stones.entry(1).or_default() += count;
+        } else {
+            let length_num = stone.ilog10() + 1 % 2;
+            if length_num % 2 == 0 {
+                *new_stones
+                    .entry(*stone / 10_usize.pow(length_num / 2))
+                    .or_default() += count;
+                *new_stones
+                    .entry(*stone % 10_usize.pow(length_num / 2))
+                    .or_default() += count;
+            } else {
+                *new_stones.entry(*stone * 2024).or_default() += count;
             }
         }
-    };
-    cache.insert(stone.clone(), r.clone());
-    r
-}
-
-fn count_stones(stones: &Number) -> usize {
-    match stones {
-        Number::Pure(_) => 1,
-        Number::Splitted(v) => v.iter().map(count_stones).sum(),
     }
+    new_stones
 }
 
 fn stars(Data { mut stones }: Data) {
-    let mut cache: HashMap<Number, Number> = HashMap::new();
-
     for n in 0..75 {
-        // println!("{cache:?}");
         if n == 25 {
-            println!("Star1: {}", count_stones(&stones))
+            println!("Star1: {}", stones.values().sum::<usize>())
         };
-        stones = evolve_stone(&stones, &mut cache);
+        stones = evolve_stone(&stones);
     }
-    println!("Star2: {}", count_stones(&stones));
+    println!("Star2: {}", stones.values().sum::<usize>());
 }
 
 fn main() -> Result<(), std::io::Error> {
